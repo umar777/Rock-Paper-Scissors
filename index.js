c59905901416a785166efb33c8007435f43c8dd4
Omar <umarbinmuhammed@gmail.com>
2023-05-30 00:19:48 +0100

    added

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    console.log('Your Choice: ' + playerSelection + ', Computer Choice: ' + computerSelection)
-    if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return 'You Win!'
-    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return 'You Lose!'
-    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        return 'You Lose!'
-    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return 'You Win!'
-    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        return 'You Lose!'
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return 'You Win!'
-    } else if (playerSelection ==  computerSelection) {
-        return 'Draw!'
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    function playRound(playerSelection, computerSelection) {
+        playerSelection = playerSelection.toLowerCase();
+        console.log('Your Choice: ' + playerSelection + ', Computer Choice: ' + computerSelection)
+        if (playerSelection == 'rock' && computerSelection == 'scissors') {
+            return 'Player'
+        } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+            return 'Computer'
+        } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+            return 'Computer'
+        } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+            return 'Player'
+        } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+            return 'Computer'
+        } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+            return 'Player'
+        } else if (playerSelection ==  computerSelection) {
+            return 'Draw!'
+        }
+      }
+    
+      function getComputerChoice() {
+        var choices = ['rock', 'paper', 'scissors'];
+        var computerChoice = choices[Math.floor(Math.random() * choices.length)];
+        return computerChoice;
+      }
+    
+    for (i = 0; i < 5; i++) {
+        const playerSelection = prompt("Please enter your choice from Rock, Papers or Scissors: ");
+        const computerSelection = getComputerChoice();
+        let winner = playRound(playerSelection, computerSelection);
+        if (winner === 'Player') {
+            console.log('You won this round');
+            playerScore++;
+        } else if (winner === 'Computer'){
+            console.log('Computer won this round')
+            computerScore++;
+        }
     }
+
+    console.log('------------------------------------')
+
+    if (playerScore > computerScore) {
+        console.log("Congratulations! You win the game!");
+    } else if (playerScore < computerScore) {
+        console.log("Sorry! Computer wins the game!");
+      } else {
+        console.log("It's a tie game!");
+      }
   }
 
-  function getComputerChoice() {
-    var choices = ['rock', 'paper', 'scissors'];
-    var computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    return computerChoice;
-  }
-
-  const playerSelection = prompt("Please enter your choice from Rock, Papers or Scissors: ");
-  const computerSelection = getComputerChoice();
-
-  console.log(playRound(playerSelection, computerSelection));
-
+game();
